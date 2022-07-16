979b9902b0438e4a1715cd8533c34949ebc8a086
Victor Leite Costa <victorleitecosta@outlook.com.br>
2022-07-16 16:45:23 -0300

    feat(prisma): prisma initial config & db

---
+++ prisma/migrations/20220716193917_init/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `User`;
+
+-- CreateTable
+CREATE TABLE `Pokemon` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `height` INTEGER NULL,
+
+    UNIQUE INDEX `Pokemon_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Images` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `pokemonId` INTEGER NOT NULL,
+    `url` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Images` ADD CONSTRAINT `Images_pokemonId_fkey` FOREIGN KEY (`pokemonId`) REFERENCES `Pokemon`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
