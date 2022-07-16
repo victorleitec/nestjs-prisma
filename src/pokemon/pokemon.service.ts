fa37e495527f8563d0667b6b932c233d5b2690bf
Victor Leite Costa <victorleitecosta@outlook.com.br>
2022-07-16 17:06:52 -0300

    feat(pokemon): add prisma service

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
 @Injectable()
 export class PokemonService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createPokemonDto: CreatePokemonDto) {
     return 'This action adds a new pokemon';
   }
